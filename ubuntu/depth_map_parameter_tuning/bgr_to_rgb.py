f18396820c60e44a29494a2a66f35180288eed16
mhubii <m.huber_1994@hotmail.de>
2019-07-28 13:32:22 +0200

    windows: added wls

--- ubuntu/depth_map_parameter_tuning/bgr_to_rgb.py
+++ ubuntu/depth_map_parameter_tuning/bgr_to_rgb.py
@@ -1,7 +1,7 @@
 import cv2
 
-l = "l_bgr_32_sadwin_13_sigma_1.0_lambda_10000.0_time_9.0ms.png"
-r = "r_bgr_32_sadwin_13_sigma_1.0_lambda_10000.0_time_9.0ms.png"
+l = "28_07_19_wls_measurements/l_bgr_32_sadwin_13_sigma_0.1_lambda_100.0_time_25.0ms.png"
+r = "28_07_19_wls_measurements/r_bgr_32_sadwin_13_sigma_0.1_lambda_100.0_time_25.0ms.png"
 
 bgr_l = cv2.imread(l)
 bgr_r = cv2.imread(r)
@@ -9,5 +9,5 @@ bgr_r = cv2.imread(r)
 rgb_l = cv2.cvtColor(bgr_l, cv2.COLOR_BGR2RGB)
 rgb_r = cv2.cvtColor(bgr_r, cv2.COLOR_BGR2RGB)
 
-cv2.imwrite("l_rgb_32_sadwin_13_sigma_1.0_lambda_10000.0_time_9.0ms.png", rgb_l)
-cv2.imwrite("r_rgb_32_sadwin_13_sigma_1.0_lambda_10000.0_time_9.0ms.png", rgb_r)
+cv2.imwrite("l_rgb_32_sadwin_13_sigma_0.1_lambda_100.0_time_25.0ms.png", rgb_l)
+cv2.imwrite("r_rgb_32_sadwin_13_sigma_0.1_lambda_100.0_time_25.0ms.png", rgb_r)
